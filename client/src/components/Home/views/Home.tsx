56d5f95b2fefe5ea7292f5f2e24c86814073ccd5
CHAPEAU Guillaume - m094866 <guillaume.chapeau@soprasteria.com>
2019-06-10 17:32:50 +0200

    feat: home page cards

--- client/src/components/Home/views/Home.tsx
+++ client/src/components/Home/views/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Dropdown, Message, Loader, Card, Progress, Icon, Modal, Button } from 'semantic-ui-react';
+import { Grid, Dropdown, Message, Loader, Card, Icon, Modal, Button, Segment, List } from 'semantic-ui-react';
 import { IProfile } from 'src/components/User/types/user';
 import { IHomeData, IEnvironment } from '../types/home';
 import { fetchHome } from '../actions/home';
@@ -8,8 +8,7 @@ import { path } from 'src/constants/path';
 
 interface IHomeState {
   user: IProfile;
-  groups: string[];
-  current: IEnvironment;
+  environments: IEnvironment[];
   isFetching: boolean;
   error: Error;
 }
@@ -17,8 +16,7 @@ interface IHomeState {
 class Home extends React.Component<{}, IHomeState> {
   public state = {
     user: {} as IProfile,
-    groups: [] as string[],
-    current: {} as IEnvironment,
+    environments: [] as IEnvironment[],
     isFetching: true,
     error: Error()
   }
@@ -26,20 +24,15 @@ class Home extends React.Component<{}, IHomeState> {
   public componentWillMount() {
     fetchHome()
       .then((data: IHomeData) => {
-        const groups = data.environments.map(env => env.group.name);
-        if (groups.length === 0) {
-          this.setState({ groups })
-        } else {
-          const current = data.environments[0];
-          this.setState({ groups, current })
-        }        
+        const environments = data.environments;
+        this.setState({ environments });
       })
       .catch(error => this.setState({ error }))
       .finally(() => this.setState({ isFetching: false }));
   }
 
   public render() {
-    const { groups, current, isFetching, error} = this.state;
+    const { environments, isFetching, error } = this.state;
 
     if (error.message) {
       return (
@@ -62,7 +55,7 @@ class Home extends React.Component<{}, IHomeState> {
       );
     }
 
-    if (groups.length === 0) {
+    if (environments.length === 0) {
       return (
         <>
           <h2>Home</h2>
@@ -70,9 +63,6 @@ class Home extends React.Component<{}, IHomeState> {
         </>
       )
     }
-
-    const filesystem = current.resources.fs[0];
-    const fsUsage = Math.round(100 * filesystem.usage / filesystem.capacity);
 
     return (
       <>
@@ -85,46 +75,75 @@ class Home extends React.Component<{}, IHomeState> {
               <Dropdown
                 selection={true}
                 multiple={true}
-                name="envID"
+                name="group"
                 placeholder="Select environment"
-                defaultValue={current.group.name}
-                options={groups.map(g => {
-                  return { text: g, value: g };
+                options={environments.map(e => {
+                  return { text: e.group.name, value: e.group.name };
                 })}
               />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            {current.group._id && (
-              <>
-                <Grid.Column width={8}>
-                  <Card fluid={true}>
-                    <Card.Content>
-                      <a href={path.groupCAdvisor.replace(":groupID", current.group._id)}>
-                        <Icon name="microchip" color="green" size="big" title={current.resources.cpu} bordered={true} floated="left" />
+            {environments.map(env => {
+              const filesystem = env.resources.fs[0];
+              const fsUsage = Math.round(100 * filesystem.usage / filesystem.capacity);
+              return (
+              <Grid.Column width={8} key={env.group.name}>
+                <Card fluid={true}>
+                  <Card.Content>
+                    <Segment as="a" href={path.groupCAdvisor.replace(":groupID", env.group._id)} floated="right">
+                      <Icon 
+                        name="microchip" 
+                        style={{ color: this.computeColor(env.resources.cpu) }} 
+                        size="big" 
+                        title={`CPU : ${env.resources.cpu}%`}
+                      />
+                      <Icon 
+                        name="server" 
+                        style={{ color: this.computeColor(env.resources.ram) }} 
+                        size="big" 
+                        title={`RAM : ${env.resources.ram}%`}
+                      />
+                      <Icon 
+                        name="hdd" 
+                        style={{ color: this.computeColor(fsUsage) }} 
+                        size="big" 
+                        title={`Disk : ${fsUsage}%`}
+                      />
+                    </Segment>
+                    <Card.Header>{env.group.name}</Card.Header>
+                    <Card.Meta>{env.daemon.host}</Card.Meta>
+                    <Card.Description>
+                      <a href={path.groupsContainers.replace(":groupID", env.group._id)}>
+                        Go to containers
                       </a>
-                      <a href={path.groupCAdvisor.replace(":groupID", current.group._id)}>
-                        <Icon name="server" color="orange" size="big" title={current.resources.ram} bordered={true} floated="left" />
-                      </a>
-                      <a href={path.groupCAdvisor.replace(":groupID", current.group._id)}>
-                        <Icon name="hdd" style={{backgroundColor: this.computeColor(fsUsage)}} size="big" title={fsUsage} bordered={true} floated="left" />
-                        <Progress percent={fsUsage} className="reverse" title={fsUsage} />
-                      </a>
-                      
-                      {current.containers.filter(c => c.State === "exited").map(c => (
-                          <Modal trigger={<Button color="red" content={c.Names[0]} labelPosition="left" icon="wheelchair" />}>
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content>
+                    <List>
+                    {env.containers.filter(c => c.State === "exited").map(c => (
+                      <List.Item>
+                        <List.Content>
+                        <Button compact={true} color="green" labelPosition="right" icon="fire extinguisher" content="Restart" floated="right" />
+                        </List.Content>
+                        <List.Content>
+                        <Icon circular={true} color="red" name="fire" />
+                          <Modal trigger={<a>{c.Names[0]}</a>}>
                             <Modal.Content style={{ background: "black", color: "white" }}>
                               <pre style={{ whiteSpace: "pre-line" }}>
-                                <TextSocket wsPath={`/api/daemons/${current.daemon._id}/docker/containers/${c.Id}/log`} />
+                                <TextSocket wsPath={`/api/daemons/${env.daemon._id}/docker/containers/${c.Id}/log`} />
                               </pre>
                             </Modal.Content>
                           </Modal>
-                      ))}
-                    </Card.Content>
-                  </Card>
-                </Grid.Column>
-              </>
-            )}
+                        </List.Content>
+                      </List.Item>
+                    ))}
+                    </List>
+                  </Card.Content>
+                </Card>
+              </Grid.Column>
+              );
+            })}
           </Grid.Row>
         </Grid>
       </>
@@ -132,9 +151,27 @@ class Home extends React.Component<{}, IHomeState> {
   }
 
   private computeColor = (percent: number) => {
-    switch (Math.round(percent/10)) {
+    switch (Math.round(percent / 10)) {
+      case 1:
+        return "#66DA81";
+      case 2:
+        return "#66DA81";
+      case 3:
+        return "#B4D95C";
+      case 4:
+        return "#B4D95C";
+      case 5:
+        return "#DDC928";
+      case 6:
+        return "#DDC928";
+      case 7:
+        return "#EFBC72";
+      case 8:
+        return "#EFBC72";
+      case 9:
+        return "#D95C5C";
       default:
-        return "D95C5C";
+        return "";
     }
   }
 }
