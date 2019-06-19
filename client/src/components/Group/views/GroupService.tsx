5a406e451a9415e26d5061ad873559f882256322
CHAPEAU Guillaume - m094866 <guillaume.chapeau@soprasteria.com>
2019-06-19 14:31:56 +0200

    fix: build failure

--- client/src/components/Group/views/GroupService.tsx
+++ client/src/components/Group/views/GroupService.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Label } from 'semantic-ui-react';
 
-import { IService } from 'src/components/Services/types/service';
+import { IService } from '../../Services/types/service';
 
 interface IGroupServiceProps {
   service: IService
@@ -9,13 +9,13 @@ interface IGroupServiceProps {
 
 export default class GroupService extends React.Component<IGroupServiceProps> {
   public render() {
-    const { service } = this.props
+    const { service } = this.props;
 
     return (
       <Card fluid={true}>
         <Card.Content>
-          <Card.Header>{service.name}</Card.Header>
-          <Card.Meta>Version</Card.Meta>
+          <Card.Header floated="left">{service.name}</Card.Header>
+          <Label>Version</Label>
           <Card.Description>
             Service stuffs
           </Card.Description>
