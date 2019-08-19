a66a10eca3adfa00e77135ee2b9c8b854d25f2b4
CHAPEAU Guillaume - m094866 <guillaume.chapeau@soprasteria.com>
2019-08-19 15:29:48 +0200

    fix: unused vars

--- client/src/components/Group/views/GroupService.tsx
+++ client/src/components/Group/views/GroupService.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, Label, Grid, Button, Icon, Dropdown, ButtonProps, Modal, DropdownProps, Menu } from 'semantic-ui-react';
+import { Card, Label, Grid, Button, Icon, Dropdown, ButtonProps, Modal } from 'semantic-ui-react';
 
 import { IGroupService } from '../../Services/types/service';
 import { copy } from '../../../utils/clipboard';
