8f75fdd9dc00ca02c65b5678d235b7d53869a60e
CHAPEAU Guillaume - m094866 <guillaume.chapeau@soprasteria.com>
2019-09-19 17:12:57 +0200

    feat: done jenkins transform

--- server/types/transform.go
+++ server/types/transform.go
@@ -8,12 +8,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// TransformJenkins converts a Docktor V1 Jenkins into Docktor V2 service
-func TransformJenkins(config types.ContainerJSON, jenkins Service) GroupService {
-
-	service := GroupService{
-		Name: "Jenkins",
+func importVariables(sub *SubService, env []string) {
+	for i := 0; i < len(sub.Variables); i++ {
+		for _, v := range env {
+			if strings.Contains(v, sub.Variables[i].Name) {
+				value := strings.Split(v, "=")[1]
+				log.Infof("Variable %s = %s", sub.Variables[i].Name, value)
+				sub.Variables[i].Value = value
+			}
+		}
 	}
+}
+
+// TransformJenkins converts a Docktor V1 Jenkins into Docktor V2 service
+func TransformJenkins(config types.ContainerJSON, jenkins Service) SubService {
 
 	image, _ := regexp.Compile(`[^:]+:([^-]+)(-[.*]+)?`)
 	version := image.FindStringSubmatch(config.Config.Image)[1]
@@ -21,8 +29,15 @@ func TransformJenkins(config types.ContainerJSON, jenkins Service) GroupService 
 	for _, sub := range jenkins.SubServices {
 		if strings.Contains(sub.File, version) {
 			log.Infof("Transforming Jenkins service %s version %s ...", config.Name, version)
+
+			// Get template
+			sub.GetVariables()
+			// Set variable values
+			importVariables(&sub, config.Config.Env)
+
+			return sub
 		}
 	}
 
-	return service
+	return SubService{}
 }
