1bbfb90961fe0482f4d5fbc5969e5e5ff99041a0
Guillaume CHAPEAU <guillaume.chapeau@soprasteria.com>
2019-11-18 16:34:58 +0100

    refactor transform

---
+++ server/handler/groups/transform.go
@@ -0,0 +1,354 @@
+package groups
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	
+	"docktor/server/storage"
+	"docktor/server/types"
+	
+	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
+)
+
+func transformServices(c echo.Context) error {
+	err := saveContainers(c)
+	if err != nil {
+		return err
+	}
+
+	group := c.Get("group").(types.Group)
+	db := c.Get("DB").(*storage.Docktor)
+
+	containerNames := ""
+	for _, c := range group.Containers {
+		containerNames += c.Name + " "
+	}
+	log.Infof("Containers to transform : %s", containerNames)
+
+	services, err := db.Services().FindAll()
+	if err != nil {
+		return err
+	}
+
+	daemon, err := db.Daemons().FindByIDBson(group.Daemon)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"daemonID": group.Daemon,
+			"error":    err,
+		}).Error("Error when retrieving group daemon")
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	findService := func(services []types.Service, name string) types.Service {
+		for _, s := range services {
+			if s.Name == name {
+				return s
+			}
+		}
+		log.Errorf("Service %s not found", name)
+		return types.Service{}
+	}
+
+	for _, conf := range group.Containers {
+		switch true {
+		case strings.Contains(conf.Config.Image, "jenkins"):
+			service := findService(services, "Jenkins")
+			serviceName, sub := types.TransformService(conf, service, "jenkins")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName}, []string{"jenkins"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "cdksonarqube"):
+			service := findService(services, "SonarQube")
+			serviceName, sub := types.TransformService(conf, service, "sonarqube")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName}, []string{"sonarqube"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "sonarqube"):
+			c, err := types.FindDependencyEnv(conf, "SONARQUBE_JDBC_URL", `jdbc:postgresql://([^:]+):([0-9]+)/[a-zA-Z]+`, 1, 2, "5432", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "SonarQube")
+			serviceName, sub := types.TransformService(conf, service, "sonarqube")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(c.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("Postgres", *c)}, []string{"sonarqube", "postgres"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "nexus"):
+			service := findService(services, "Nexus")
+			serviceName, sub := types.TransformService(conf, service, "nexus")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName}, []string{"nexus"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "cdkintools2"):
+			mongo, err := types.FindDependencyEnv(conf, "MONGO_URL", `mongodb://([^:]+):([0-9]+)`, 1, 2, "27017", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			redis, err := types.FindDependencyEnv(conf, "REDIS_URL", `([^:]+):([0-9]+)`, 1, 2, "6379", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "Intools")
+			serviceName, sub := types.TransformService(conf, service, "Intools2")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(mongo.Name, redis.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("MongoDB", *mongo)}, []string{"intools", "mongo"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "cdkintools"):
+			service := findService(services, "Intools")
+			serviceName, sub := types.TransformService(conf, service, "intools")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName}, []string{"intools"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "intools"):
+			mongo, err := types.FindDependencyEnv(conf, "MONGO_SERVER", `([^:]+):([0-9]+)`, 1, 2, "27017", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			redis, err := types.FindDependencyEnv(conf, "REDIS_HOST", `([^:]+):([0-9]+)`, 1, 2, "6379", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "Intools")
+			serviceName, sub := types.TransformService(conf, service, "intools")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(mongo.Name, redis.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("MongoDB", *mongo)}, []string{"intools", "mongo"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "phabricator"):
+			service := findService(services, "Phabricator")
+			serviceName, sub := types.TransformService(conf, service, "phabricator")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName}, []string{"phabricator"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "rundeck"):
+			databaseEnv := "RUNDECK_DATABASE_URL"
+			if strings.Contains(conf.Config.Image, "cdkrundeck") {
+				databaseEnv = "DATABASE_URL"
+			}
+			c, err := types.FindDependencyEnv(conf, databaseEnv, `jdbc:postgresql://([^:]+):([0-9]+)/[a-zA-Z]+`, 1, 2, "5432", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "Rundeck")
+			serviceName, sub := types.TransformService(conf, service, "rundeck")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(c.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("Postgres", *c)}, []string{"rundeck", "postgres"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "ctm"):
+			esURL := ":"
+			for _, env := range conf.Config.Env {
+				if strings.Contains(env, "ES_HOST") {
+					esURL = strings.Split(env, "=")[1] + esURL
+				}
+				if strings.Contains(env, "ES_PORT") {
+					esURL += strings.Split(env, "=")[1]
+				}
+			}
+			es, err := types.FindDependency(esURL, `([^:]+):([0-9]+)`, 1, 2, "9300", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "CTM")
+			serviceName, sub := types.TransformService(conf, service, "ctm")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(es.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("elasticsearch", *es)}, []string{"ctm", "elasticsearch"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "release-monitoring-2"):
+			esURL := ":"
+			for _, env := range conf.Config.Env {
+				if strings.Contains(env, "ES_HOST") {
+					esURL = strings.Split(env, "=")[1] + esURL
+				}
+				if strings.Contains(env, "ES_PORT") {
+					esURL += strings.Split(env, "=")[1]
+				}
+			}
+			es, err := types.FindDependency(esURL, `([^:]+):([0-9]+)`, 1, 2, "9300", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "ReleaseMonitoring2")
+			serviceName, sub := types.TransformService(conf, service, "release-monitoring")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(es.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("elasticsearch", *es)}, []string{"release-monitoring", "elasticsearch"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		case strings.Contains(conf.Config.Image, "cdkplaylogs"):
+			esURL := ":"
+			for _, env := range conf.Config.Env {
+				if strings.Contains(env, "ELASTICSEARCH_URL") {
+					esURL = strings.Split(env, "=")[1] + esURL
+				}
+				if strings.Contains(env, "ELASTICSEARCH_REST_API_PORT") {
+					esURL += strings.Split(env, "=")[1]
+				}
+			}
+			es, err := types.FindDependency(esURL, `([^:]+):([0-9]+)`, 1, 2, "9200", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			mongo, err := types.FindDependencyEnv(conf, "MONGO_URL", `([^:]+):([0-9]+)`, 1, 2, "27017", group.Containers)
+			if err != nil {
+				log.Error(err.Error())
+				break
+			}
+			service := findService(services, "Playlogs")
+			serviceName, sub := types.TransformService(conf, service, "playlogs")
+			group, err = createGroupService(service, sub, serviceName, group, daemon, db)
+			if err != nil {
+				log.Errorf("Failed to create group service for %s", conf.Name)
+			}
+			errs := daemon.RemoveContainers(es.Name, mongo.Name, conf.Name)
+			if len(errs) > 0 {
+				return fmt.Errorf("%+v", errs)
+			}
+			err = types.MoveVolumes(serviceName, []string{serviceName, types.FindServiceName("elasticsearch", *es), types.FindServiceName("MongoDB", *mongo)}, []string{"playlogs", "elasticsearch", "mongo"}, group.Name, daemon)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			break
+		// TODO : case Shinken, Zap, SSO
+		default:
+			log.Warningf("No match found for image : %s", conf.Config.Image)
+		}
+	}
+
+	return c.JSON(http.StatusOK, group)
+}
+
+func handleJenkinsTransform() {
+
+}
+
+func createGroupService(service types.Service, sub types.SubService, serviceName string, group types.Group, daemon types.Daemon, db *storage.Docktor) (types.Group, error) {
+	groupService, err := sub.ConvertToGroupService(serviceName, daemon, service, group, false)
+	if err != nil {
+		return types.Group{}, err
+	}
+	group.Services = append(group.Services, groupService)
+	group, err = db.Groups().Save(group)
+	if err != nil {
+		log.Errorln("Error when saving group. Copy the JSON manually into the DB :")
+		log.Errorln(groupService)
+	}
+	return group, nil
+}
--- server/types/transform.go
+++ server/types/transform.go
@@ -10,6 +10,71 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// FindServiceName gets the a-priori unique name of a service based on mapped volumes
+func FindServiceName(init string, config types.ContainerJSON) string {
+	serviceName := init
+	for _, mount := range config.Mounts {
+		if strings.Contains(mount.Source, "/data/") {
+			serviceName = strings.Split(mount.Source, "/")[3]
+		}
+	}
+	return serviceName
+}
+
+// FindDependencyEnv finds the environment variable that contains the dependency data
+func FindDependencyEnv(conf types.ContainerJSON, refEnv string, refExpr string, nameMatchIndex int, portMatchIndex int, defaultPortValue string, containers []types.ContainerJSON) (*types.ContainerJSON, error) {
+	for _, env := range conf.Config.Env {
+		if strings.Contains(env, refEnv) {
+			return FindDependency(strings.SplitN(env, "=", 2)[1], refExpr, nameMatchIndex, portMatchIndex, defaultPortValue, containers)
+		}
+	}
+	return nil, fmt.Errorf("Couldn't find dependency variable in %s", conf.Name)
+}
+
+// FindDependency finds the container in a set which is the wanted dependency based on env variables
+func FindDependency(expr string, refExpr string, nameMatchIndex int, portMatchIndex int, defaultPortValue string, containers []types.ContainerJSON) (*types.ContainerJSON, error) {
+	var depName, depPort string
+
+	r, _ := regexp.Compile(refExpr)
+	match := r.FindStringSubmatch(expr)
+	depName = match[nameMatchIndex]
+	depPort = match[portMatchIndex]
+
+	if depPort == "" || depName == "" {
+		return nil, fmt.Errorf("Couldn't find dependency location in %s variable", expr)
+	} else if depPort == defaultPortValue {
+		for _, c := range containers {
+			if strings.TrimPrefix(c.Name, "/") == depName {
+				return &c, nil
+			}
+		}
+		return nil, fmt.Errorf("Couldn't find dependency named %s", depName)
+	} else {
+		port, _ := nat.NewPort("tcp", defaultPortValue)
+		for _, c := range containers {
+			if len(c.HostConfig.PortBindings[port]) != 0 {
+				externalPort := c.HostConfig.PortBindings[port][0].HostPort
+				if externalPort == depPort {
+					return &c, nil
+				}
+			}
+		}
+		return nil, fmt.Errorf("Couldn't find container on port %s", depPort)
+	}
+}
+
+func findVersion(image string) string {
+	r, _ := regexp.Compile(`[^:]+:([^-]+)(-[.*]+)?`)
+	return r.FindStringSubmatch(image)[1]
+}
+
+// TransformService converts a Docktor V1 into Docktor V2 service
+func TransformService(config types.ContainerJSON, service Service, defaultName string) (string, SubService) {
+	serviceName := FindServiceName(defaultName, config)
+	version := findVersion(config.Config.Image)
+	return toSubService(serviceName, version, config, service)
+}
+
 func importVariables(sub *SubService, env []string) {
 	for i := 0; i < len(sub.Variables); i++ {
 		match := false
@@ -32,78 +97,6 @@ func importVariables(sub *SubService, env []string) {
 	}
 }
 
-// FindServiceName gets the a-priori unique name of a service based on mapped volumes
-func FindServiceName(init string, config types.ContainerJSON) string {
-	serviceName := init
-	for _, mount := range config.Mounts {
-		if strings.Contains(mount.Source, "/data/") {
-			serviceName = strings.Split(mount.Source, "/")[3]
-		}
-	}
-	return serviceName
-}
-
-func findVersion(image string) string {
-	r, _ := regexp.Compile(`[^:]+:([^-]+)(-[.*]+)?`)
-	return r.FindStringSubmatch(image)[1]
-}
-
-// TransformJenkins converts a Docktor V1 Jenkins into Docktor V2 service
-func TransformJenkins(config types.ContainerJSON, jenkins Service) (string, SubService) {
-	serviceName := FindServiceName("jenkins", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, jenkins)
-}
-
-// TransformSonarLegacy converts a Docktor V1 Sonar legacy into Docktor V2 service
-func TransformSonarLegacy(config types.ContainerJSON, sonar Service) (string, SubService) {
-	serviceName := FindServiceName("sonarqube_legacy", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, sonar)
-}
-
-// TransformPhabricator converts a Docktor V1 Phabricator into Docktor V2 service
-func TransformPhabricator(config types.ContainerJSON, phab Service) (string, SubService) {
-	serviceName := FindServiceName("phabricator", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, phab)
-}
-
-// TransformNexus converts a Docktor V1 Nexus into Docktor V2 service
-func TransformNexus(config types.ContainerJSON, nexus Service) (string, SubService) {
-	serviceName := FindServiceName("nexus", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, nexus)
-}
-
-// TransformSonarqube converts a Docktor V1 Sonarqube into Docktor V2 service
-func TransformSonarqube(config types.ContainerJSON, database types.ContainerJSON, sonarqube Service) (string, SubService) {
-	serviceName := FindServiceName("sonarqube", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, sonarqube)
-}
-
-// TransformIntools3 converts a Docktor V1 Intools 3 into Docktor V2 service
-func TransformIntools3(config types.ContainerJSON, mongo types.ContainerJSON, redis types.ContainerJSON, intools Service) (string, SubService) {
-	serviceName := FindServiceName("intools", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, intools)
-}
-
-// TransformIntools2 converts a Docktor V1 Intools 2 into Docktor V2 service
-func TransformIntools2(config types.ContainerJSON, mongo types.ContainerJSON, redis types.ContainerJSON, intools Service) (string, SubService) {
-	serviceName := FindServiceName("intools2", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, intools)
-}
-
-// TransformIntools1 converts a Docktor V1 Intools 1 into Docktor V2 service
-func TransformIntools1(config types.ContainerJSON, intools Service) (string, SubService) {
-	serviceName := FindServiceName("intools1", config)
-	version := findVersion(config.Config.Image)
-	return toSubService(serviceName, version, config, intools)
-}
-
 func toSubService(serviceName string, version string, config types.ContainerJSON, service Service) (string, SubService) {
 	for _, sub := range service.SubServices {
 		if strings.Contains(sub.File, version) {
@@ -120,7 +113,7 @@ func toSubService(serviceName string, version string, config types.ContainerJSON
 // MoveVolumes changes the path of a container's mapped volume
 func MoveVolumes(serviceName string, sources []string, targets []string, groupName string, daemon Daemon) (err error) {
 	sourceVolume := fmt.Sprintf("%s/%s", daemon.Docker.Volume, groupName)
-	tmpServiceDir := fmt.Sprintf("%s-v2", serviceName)
+	tmpServiceDir := fmt.Sprintf("%s-%s", serviceName, randString(8))
 
 	// Initialize commands
 	cdCmd := "cd /data"
@@ -138,38 +131,4 @@ func MoveVolumes(serviceName string, sources []string, targets []string, groupNa
 	err = daemon.CmdContainer(sourceVolume, cmd)
 
 	return
-}
-
-// FindDependency finds the container in a set which is the wanted dependency based on env variables
-func FindDependency(conf types.ContainerJSON, refEnv string, refExpr string, nameMatchIndex int, portMatchIndex int, defaultPortValue string, containers []types.ContainerJSON) (*types.ContainerJSON, error) {
-	var depName, depPort string
-	for _, env := range conf.Config.Env {
-		if strings.Contains(env, refEnv) {
-			r, _ := regexp.Compile(refExpr)
-			match := r.FindStringSubmatch(strings.Split(env, "=")[1])
-			depName = match[nameMatchIndex]
-			depPort = match[portMatchIndex]
-		}
-	}
-	if depPort == "" {
-		return nil, fmt.Errorf("Couldn't find dependency port in %s variables", conf.Name)
-	} else if depPort == defaultPortValue {
-		for _, c := range containers {
-			if strings.TrimPrefix(c.Name, "/") == depName {
-				return &c, nil
-			}
-		}
-		return nil, fmt.Errorf("Couldn't find dependency named %s", depName)
-	} else {
-		port, _ := nat.NewPort("tcp", defaultPortValue)
-		for _, c := range containers {
-			if len(c.HostConfig.PortBindings[port]) != 0 {
-				externalPort := c.HostConfig.PortBindings[port][0].HostPort
-				if externalPort == depPort {
-					return &c, nil
-				}
-			}
-		}
-		return nil, fmt.Errorf("Couldn't find container on port %s", depPort)
-	}
 }
